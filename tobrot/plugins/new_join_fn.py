66a92c19fb04821d8032717ba87ba4f596a55123
Rdx98 <59723048+Redkillerbot@users.noreply.github.com>
2020-08-21 08:21:31 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,12 +33,12 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    # await message.reply_text("**Oh!! You Are Looking For Commands Use** `/Commands` To See..", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""**Welcome To BlinkCloud Enjoy Leeching.**\n\n **For Accessing Team Drive Join Our Google Group **: <a href="https://groups.google.com/forum/m/#!forum/blink-cloud">BlinkCloud TD</a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
